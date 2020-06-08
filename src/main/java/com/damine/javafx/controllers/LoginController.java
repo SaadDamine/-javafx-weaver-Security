9a8f33bf430d025ea53c6c085260e0079438e8c0
SaadDamine <m.sdaminesaad@gmail.com>
2020-06-08 07:38:59 +0100

    add notification warning to fxml Login

--- src/main/java/com/damine/javafx/controllers/LoginController.java
+++ src/main/java/com/damine/javafx/controllers/LoginController.java
@@ -9,6 +9,7 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.geometry.Pos;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
@@ -17,6 +18,7 @@ import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 import net.rgielen.fxweaver.core.FxmlView;
+import org.controlsfx.control.Notifications;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -45,7 +47,7 @@ public class LoginController implements Initializable {
         role.getItems().add(RoleEnum.USER.name());
         role.getItems().add(RoleEnum.ADMINISTRATOR.name());
         role.getSelectionModel().select(RoleEnum.USER.name());
-        if (userService.testUser()){
+        if (userService.testUser()) {
             // Ceci n'est pas à recopier en production
             List<RoleEnum> userRole = Arrays.asList(RoleEnum.USER);
             List<RoleEnum> adminRole = Arrays.asList(RoleEnum.USER, RoleEnum.ADMINISTRATOR);
@@ -58,18 +60,24 @@ public class LoginController implements Initializable {
 
     @FXML
     public void login(ActionEvent event) throws Exception {
-        if(username.getText()==null){
+        if (username.getText().isEmpty()) {
+            Notifications notifications = Notifications.create().title("Error").text("insert user name").graphic(null).position(Pos.CENTER);
+            notifications.showWarning();
             return;
         }
-        if(password.getText()==null){
+        if (password.getText().isEmpty()) {
+            Notifications notifications = Notifications.create().title("Error").text("insert user password").graphic(null).position(Pos.CENTER);
+            notifications.showWarning();
             return;
         }
-        if(userService.loadUserByUsername(username.getText()) == null){
+        if (userService.loadUserByUsername(username.getText()) == null) {
+            Notifications notifications = Notifications.create().title("Error").text("user not found").graphic(null).position(Pos.CENTER);
+            notifications.showWarning();
             return;
         }
         if (BCryptManagerUtil.passwordencoder().matches(password.getText(), userService.loadUserByUsername(username.getText()).getPassword())) {
-            if(role.getSelectionModel().getSelectedItem().equals(RoleEnum.USER.name())){
-                if(userService.userHasAuthority((User) userService.loadUserByUsername(username.getText()),RoleEnum.USER.name())){
+            if (role.getSelectionModel().getSelectedItem().equals(RoleEnum.USER.name())) {
+                if (userService.userHasAuthority((User) userService.loadUserByUsername(username.getText()), RoleEnum.USER.name())) {
                     Node node = (Node) event.getSource();
                     Stage stage = (Stage) node.getScene().getWindow();
                     Scene scene = stage.getScene();
@@ -77,19 +85,27 @@ public class LoginController implements Initializable {
                     Parent root = (Parent) fxmlLoader.load();
                     scene.setRoot(root);
                 }
+            } else {
+                Notifications notifications = Notifications.create().title("Error").text("permission denied").graphic(null).position(Pos.CENTER);
+                notifications.showWarning();
+                return;
             }
-            if(role.getSelectionModel().getSelectedItem().equals(RoleEnum.ADMINISTRATOR.name())){
-                if(userService.userHasAuthority((User) userService.loadUserByUsername(username.getText()),RoleEnum.ADMINISTRATOR.name())){
+            if (role.getSelectionModel().getSelectedItem().equals(RoleEnum.ADMINISTRATOR.name())) {
+                if (userService.userHasAuthority((User) userService.loadUserByUsername(username.getText()), RoleEnum.ADMINISTRATOR.name())) {
                     Node node = (Node) event.getSource();
                     Stage stage = (Stage) node.getScene().getWindow();
                     Scene scene = stage.getScene();
                     FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("../fxml/admin.fxml"));
                     Parent root = (Parent) fxmlLoader.load();
                     scene.setRoot(root);
+                } else {
+                    Notifications notifications = Notifications.create().title("Error").text("permission denied").graphic(null).position(Pos.CENTER);
+                    notifications.showWarning();
                 }
             }
-        }else{
-            System.out.println("encoder: false");
+        } else {
+            Notifications notifications = Notifications.create().title("Error").text("wrong password or username").graphic(null).position(Pos.CENTER);
+            notifications.showWarning();
         }
     }
 }
